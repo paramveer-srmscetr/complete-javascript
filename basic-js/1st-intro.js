bbb29d88fa39d0c711e76843c0dcfc26257245fe
Paramveer singh <82169480+paramveer-srmscetr@users.noreply.github.com>
2023-09-16 08:13:40 +0000

    conatiner

--- basic-js/1st-intro.js
+++ basic-js/1st-intro.js
@@ -1,1 +1,14 @@
-console.log("ishu")
+//console.log("ishu")
+
+//variables
+
+const accountId=144453
+let accountemail="ishu@gmail.com"
+var accountPassword="q3343"
+accountCity="delhi"
+//accountId=2 // not allowed
+console.log(accountId);
+console.log(accountPassword);
+console.table([accountId,accountPassword,accountemail,accountCity]);
+
+
